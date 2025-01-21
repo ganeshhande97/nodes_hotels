cd43d08ab7c2a9cc59b3fa5bbb21446c18c407a5
Ganesh Hande <ganeshhande97@gmail.com>
2025-01-21 09:57:55 +0530

    first version of our hotels

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const db = require('./DB.JS');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.get('/', function (_req, res) {
+  res.send('Welcome to my hotel... how i can help you ?,we have list of menu')  
+});
+
+const menuRoutes = require('./routes/menuRoutes');
+const personRoutes = require('./routes/personRoutes');
+
+
+app.use('/person',personRoutes);
+app.use('/menu',menuRoutes);
+
+app.listen(3000,()=>{
+    console.log('Listening on port 3000');
+});
