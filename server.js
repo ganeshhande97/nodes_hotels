035bdabf50372977cc7180e02eda11c0c7007411
Ganesh Hande <ganeshhande97@gmail.com>
2025-01-21 14:40:20 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const db = require('./DB.JS');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -16,6 +17,8 @@ const personRoutes = require('./routes/personRoutes');
 
 app.use('/person',personRoutes);
 app.use('/menu',menuRoutes);
+
+
 
 app.listen(3000,()=>{
     console.log('Listening on port 3000');
